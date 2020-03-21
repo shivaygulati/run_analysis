6b13c071bff98f794bae89c336b70c4e7042359e
shivaygulati <53039471+shivaygulati@users.noreply.github.com>
2020-03-22 01:14:07 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,42 +1,81 @@
+# Import the dplyr library
 library(dplyr)
-filename<-"project.zip"
-> if (!file.exists(filename)){
-  +     fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-  +     download.file(fileURL, filename, method="curl")
-  + }  
-if (!file.exists("UCI HAR Dataset")) { 
-  +     unzip(filename) 
-  + }
-> features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
-> activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
-> subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-> x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
-> y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
-> subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-> x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
-> y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
-> X <- rbind(x_train, x_test)
-> Y <- rbind(y_train, y_test)
-> Subject <- rbind(subject_train, subject_test)
-> Merged_Data <- cbind(Subject, Y, X)
-> TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
-> TidyData$code <- activities[TidyData$code, 2]
-> names(TidyData)[2] = "activity"
-> names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
-> names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
-> names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-> names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-> names(TidyData)<-gsub("^t", "Time", names(TidyData))
-> names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-> names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
-> names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-> names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
-> names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
-> names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-> names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
-> 
-  > FinalData <- TidyData %>%
-    +     group_by(subject, activity) %>%
-    +     summarise_all(funs(mean))
-  write.table(FinalData, "FinalData.txt", row.name=FALSE)
-  > str(FinalData)
+
+# Read in the X test dataset
+x.test <- read.csv("UCI HAR Dataset/test/X_test.txt", sep="",
+                              header=FALSE)
+
+# Read in the test labels
+y.test <- read.csv("UCI HAR Dataset/test/y_test.txt", sep="",
+                              header=FALSE)
+
+# Rest in the test subject dataset
+subject.test <- read.csv("UCI HAR Dataset/test/subject_test.txt",
+                                    sep="", header=FALSE)
+
+# Merge the test datasets into a single dataframe
+test <- data.frame(subject.test, y.test, x.test)
+
+# Read in the X training dataset
+x.train <- read.csv("UCI HAR Dataset/train/X_train.txt", sep="",
+                               header=FALSE)
+
+# Read in the training labels
+y.train <- read.csv("UCI HAR Dataset/train/y_train.txt", sep="",
+                               header=FALSE)
+
+# Read in the training subject dataset
+subject.train <- read.csv("UCI HAR Dataset/train/subject_train.txt",
+                                    sep="", header=FALSE)
+
+# Merge test training datasets into a single dataframe
+train <- data.frame(subject.train, y.train, x.train)
+
+# Combine the training and test running datasets
+run.data <- rbind(train, test)
+
+# Remove the files we don't need anymore from
+# the environment.
+remove(subject.test, x.test, y.test, subject.train,
+       x.train, y.train, test, train)
+
+# Read in the measurement labels dataset
+features <- read.csv("UCI HAR Dataset/features.txt", sep="", header=FALSE)
+# Convert the 2nd column into a vector
+column.names <- as.vector(features[, 2])
+# Apply the measurement labels as column names to the combined
+# running dataset
+colnames(run.data) <- c("subject_id", "activity_labels", column.names)
+
+# Select only the columns that contain mean or standard deviations.
+# Make sure to bring along the subject and label columns.
+# Exclude columns with freq and angle in the name.
+run.data <- select(run.data, contains("subject"), contains("label"),
+                       contains("mean"), contains("std"), -contains("freq"),
+                       -contains("angle"))
+
+# Read in the activity labels dataset
+activity.labels <- read.csv("UCI HAR Dataset/activity_labels.txt", 
+                            sep="", header=FALSE)
+
+# Replace the activity codes in the trimmed down running
+# dataset with the labels from the activity labels dataset.
+run.data$activity_labels <- as.character(activity.labels[
+  match(run.data$activity_labels, activity.labels$V1), 'V2'])
+
+# Clean up the column names. Remove parantheses and hyphens
+# from column names, both of which are invalid characters in
+# column names. Also fix a set of columns that repeat the
+# word "Body".
+setnames(run.data, colnames(run.data), gsub("\\(\\)", "", colnames(run.data)))
+setnames(run.data, colnames(run.data), gsub("-", "_", colnames(run.data)))
+setnames(run.data, colnames(run.data), gsub("BodyBody", "Body", colnames(run.data)))
+
+# Group the running data by subject and activity, then
+# calculate the mean of every measurement.
+run.data.summary <- run.data %>%
+  group_by(subject_id, activity_labels) %>%
+  summarise_each(funs(mean))
+
+# Write run.data to file
+write.table(run.data.summary, file="run_data_summary.txt", row.name=FALSE)
